7f1979ccba1f3e39831a62c1ab17b169b7f13b9d
Andrew Harris <blakebharris@gmail.com>
2018-01-16 15:08:20 -0600

    first commit

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@ const Web3 = require('web3');
 const {interface, bytecode} = require('./compile');
 
 const provider = new HDWalletProvider(
-    'words for metamask',
-    'infura api '
+    '',
+    ''
 );
 
 const web3 = new Web3(provider);
@@ -12,12 +12,12 @@ const web3 = new Web3(provider);
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from: ' + accounts[0]);
-
+    //update to reference initial value that gets put in the constructor.  deploy({data: bytecode, arguments: []})
     const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi There!']})
+        .deploy({ data: bytecode })
         .send({ gas: '1000000', from: accounts[0] });
-    
-    console.log('contract deployed to: ', result.options.address);
 
+    console.log(interface);
+    console.log('contract deployed to: ', result.options.address);
 };
 deploy();
